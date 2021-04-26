cdb99ab7a038b558f3e2904ed92ee057eb53ba0a
lubnaabbas <lubnaabbasabbas@gmail.com>
2021-04-26 16:17:42 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+var $links = $(".itemLinks");
+
+$links.click(function(e){
+  $links.removeclass("active");
+  var clikedLink = e.target;
+  clikedLink = $(clikedLink);
+  var position = clikedLink.attr("data-pos");
+  var translatevalue = "translate3d ("+ position * 25 +"%, 0px, 0px)";
+  $("#wrapper").css({
+      transform: translatevalue
+  });
+
+  clikedLink.addclass("active");
+
+});
+
+$($links[0]).addclass("active");
